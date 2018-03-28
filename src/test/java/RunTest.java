685c8d37c9c4457ec2290b3e16769abc105db0c5
Bhavika Patel <bhavikapatel@Bhavikas-MacBook-Air.local>
2018-03-28 16:42:41 +0100

    First Project

---
+++ src/test/java/RunTest.java
@@ -0,0 +1,9 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = ".",tags="@Sp1")
+public class RunTest {
+}
